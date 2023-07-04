cd29df9bc457d5f106bd46c7f6e166aceb8e6057
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-04 13:50:22 +0545

    Add project files.

---
+++ Areas/Admin/Controllers/BusController.cs
@@ -0,0 +1,99 @@
+﻿using BusTicketBooking.DataAccess.Infrastructure;
+using BusTicketBooking.DataAccess.Repositories;
+using BusTicketBooking.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusTicketBooking.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BusController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BusController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region APICALL
+        public IActionResult GetAllBus()
+        {
+            var bus = _unitOfWork.BusRepository.GetAll();
+            return Json(new { data = bus });
+        }
+
+        #endregion
+
+        #region CreateUpdateAPICALL
+        public IActionResult CreateUpdate(int? id)
+        {
+            BusVm vm = new BusVm();
+            if (id == null || id == 0)
+            {
+                return View(vm);
+            }
+            else
+            {
+                vm.Bus = _unitOfWork.BusRepository.GetFirstOrDefault(x => x.Id == id);
+                if (vm == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(BusVm vm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (vm.Bus.Id == 0)
+                {
+                    _unitOfWork.BusRepository.Insert(vm.Bus);
+                    TempData["Success"] = "Bus Created Done!";
+                }
+                else
+                {
+                    _unitOfWork.BusRepository.Update(vm.Bus);
+                    TempData["Success"] = "Bus Update Done!";
+                }
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        #endregion
+
+
+        #region DeleteAPICALL
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var bus = _unitOfWork.BusRepository.GetFirstOrDefault(x => x.Id == id);
+            if (bus == null)
+            {
+                return Json(new { success = false, message = "Error in Featching Data" });
+            }
+            else
+            {
+                _unitOfWork.BusRepository.Delete(bus);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Bus Delated" });
+            }
+        }
+
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ wwwroot/js/Bus.js
@@ -0,0 +1,47 @@
+﻿var dtable;
+$(document).ready(function () {
+    dtable = $('#myTable').DataTable({
+        "ajax": { "url": "Admin/Bus/GetAllBus" },
+        "colums": [
+
+        { "data": "busNumber" },
+        { "data": "seatCapacity" },
+
+        {
+            "data": "id",
+            "render": function (data) {
+                return
+                <a href="/Admin/Bus/CreateUpdate?id="${data}"><i class="bi bi - pencil - square"> </i> </a>
+                <a onclick = RemoveProduct("/Admin/Bus/Delete/${data}") <i class="bi bi-trash"> </i> </a>
+            }
+        }
+        ]
+    });
+});
+function RemoveProduct(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancalButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dtable.ajax.reload();
+                        toastr.success(data.message);
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
