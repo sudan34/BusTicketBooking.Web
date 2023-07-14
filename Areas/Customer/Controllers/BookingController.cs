0a72897e51c325418ee34a97e0c86c962cd394be
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-14 15:00:17 +0545

    added

--- Areas/Customer/Controllers/BookingController.cs
+++ Areas/Customer/Controllers/BookingController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusTicketBooking.DataAccess.Infrastructure;
+using BusTicketBooking.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
 
 namespace BusTicketBooking.Web.Areas.Customer.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public BookingController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
